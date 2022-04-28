d62b1ec9e558f0eaf77daa8b4e458bcf270d4c1f
Nicolas Hormazabal <nhormazabal@geovictoria.com>
2022-04-27 22:47:50 -0400

    ejemplos comunicacion frame

---
+++ clase07/ejemploFrame/src/main/java/claseunab/ejemploframe/Observable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package claseunab.ejemploframe;
+
+/**
+ *
+ * @author luster
+ */
+public interface Observable {
+    public void addObserver(Observer observer);
+}
