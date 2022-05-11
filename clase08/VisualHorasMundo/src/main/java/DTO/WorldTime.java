99f0e960e25ee9372f0475befe53c8a19bfa5bc6
Nicolas Hormazabal <nhormazabal@geovictoria.com>
2022-05-11 19:00:42 -0400

    Ejemplo java tiempo

---
+++ clase08/VisualHorasMundo/src/main/java/DTO/WorldTime.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package claseunab.horasmundo.DTO;
+
+/**
+ *
+ * @author luster
+ */
+public class WorldTime {
+
+    public String abbreviation;
+    public String client_ip;
+    public String datetime;
+    public float day_of_week;
+    public float day_of_year;
+    public boolean dst;
+    public String dst_from;
+    public float dst_offset;
+    public String dst_until;
+    public float raw_offset;
+    public String timezone;
+    public float unixtime;
+    public String utc_datetime;
+    public String utc_offset;
+    public float week_number;
+}
