1800b10e49d6a1b5d14d81a04a93ed6f34f080dc
Matias <mogreco@uc.cl>
2022-05-18 19:50:01 -0400

    uso de timer en horasmundo

---
+++ clase08/horasMundoTimer/GUI2/src/main/java/com/mycompany/gui2/HorasRespuesta.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gui2;
+
+/**
+ *
+ * @author Alumno
+ */
+public class HorasRespuesta {
+    public String abbreviation; 
+    public String datetime; //	"2022-05-12T01:54:56.389449+02:00"
+    public String client_ip; //	"190.208.45.244"
+    public int day_of_week;//	4
+    public int day_of_year;//	132
+    public Boolean dst; //	true
+    public String dst_from;//	"2022-03-27T01:00:00+00:00"
+    public int dst_offset;//	3600
+    public String dst_until;//	"2022-10-30T01:00:00+00:00"
+    public int raw_offset;//	3600
+    public String timezone; //	"Europe/Berlin"
+    public int unixtime;//	1652313296
+    public String utc_datetime;//	"2022-05-11T23:54:56.389449+00:00"
+    public String utc_offset; //	"+02:00"
+    public int week_number;//	19
+    
+}
---
+++ clase08/horasMundoTimer/GUI2/src/main/java/com/mycompany/gui2/gui2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gui2;
+
+/**
+ *
+ * @author Alumno
+ */
+public class gui2 {
+    public static void main(String[] args){
+        interfaz i = new interfaz();
+        i.setVisible(true);
+    }
+    
+}
